a44dab8a9bca79fe7457448d4f00b78b6f33861a
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-01-03 00:27:38 +0200

    task 1-1 finished

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+const listName = document.getElementById('categories');
+//console.log(listName);
+const listItems = document.querySelectorAll('.item');
+//console.log(listItem);
+const listItemNumber  = listItems.length;
+console.log('Number of categories: ' + listItemNumber);
