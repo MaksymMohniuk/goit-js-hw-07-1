af2cc7dd018362b4c398fe79ed82e4ad1cfba3ab
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-01-03 17:34:20 +0200

    finish-task-1

--- js/task-1.js
+++ js/task-1.js
@@ -4,3 +4,14 @@ const listItems = document.querySelectorAll('.item');
 //console.log(listItem);
 const listItemNumber  = listItems.length;
 console.log('Number of categories: ' + listItemNumber);
+
+//const listItem = document.querySelector('.item');
+//console.log(listItem);
+listItems.forEach ((listItem) => {
+    const listName = listItem.querySelector('h2').textContent;
+    const listCategory = listItem.querySelectorAll('ul li');
+    console.log(`Category: ${listName} 
+    Elements: ${listCategory.length}`)
+}
+
+) 
