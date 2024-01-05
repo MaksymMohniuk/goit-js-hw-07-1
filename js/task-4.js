e2c48267286d1ec40edc1cf4c4a47844ad8850df
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-01-05 15:32:06 +0200

    finish-task-7-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const mail = e.target.elements.email.value.trim();
+    const password = e.target.elements.password.value.trim();
+    if(mail === '' || password === '') {
+        alert('All form fields must be filled in')
+        return
+    }
+    const data = {mail, 
+    password
+    } 
+    console.log(data);
+    form.reset();
+})
